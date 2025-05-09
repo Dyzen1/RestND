0743799e8041465ae2fb16c21fff6334d3137548
Dyzen1 <117169978+Dyzen1@users.noreply.github.com>
2025-05-09 21:19:11 +0300

    daddy fixed it

--- RestND/Validations/TableValidator.cs
+++ RestND/Validations/TableValidator.cs
@@ -1,112 +1,114 @@
-﻿using RestND.Data;
-using RestND.MVVM.Model.Tables;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using RestND.MVVM.Model.Tables;
+﻿//using RestND.Data;
+//using RestND.MVVM.Model.Tables;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using RestND.MVVM.Model.Tables;
 
-namespace RestND.Validations
-{
-<<<<<<< HEAD
-    public class TableValidator
-    {
+//namespace RestND.Validations
+//{
 
-        private readonly Table _table;
-        private readonly TableServices _tableService;
+//    public class TableValidator
+//    {
 
-        public TableValidator(Table table)
-        {
-            _table = table;
-            _tableService = new TableServices();
-        }
+//        private readonly Table _table;
+//        private readonly TableServices _tableService;
 
-        public bool ValidateTableNumber(out string errorMessage)
-        {
-            errorMessage = string.Empty;
+//        public TableValidator(Table table)
+//        {
+//            _table = table;
+//            _tableService = new TableServices();
+//        }
 
-            if (_table.Table_Number <= 0)
-            {
-                errorMessage = "Table number must be greater than 0!";
-                return false;
-            }
+//        public bool ValidateTableNumber(out string errorMessage)
+//        {
+//            errorMessage = string.Empty;
 
-            List<Table> existingTables = _tableService.GetAll();
+//            if (_table.Table_Number <= 0)
+//            {
+//                errorMessage = "Table number must be greater than 0!";
+//                return false;
+//            }
 
-            if (existingTables.Any(t => t.Table_Number == _table.Table_Number && t.Table_ID != _table.Table_ID))
-            {
-                errorMessage = "Table number already exists!";
-                return false;
-            }
+//            List<Table> existingTables = _tableService.GetAll();
 
-            return true;
-        }
+//            if (existingTables.Any(t => t.Table_Number == _table.Table_Number && t.Table_ID != _table.Table_ID))
+//            {
+//                errorMessage = "Table number already exists!";
+//                return false;
+//            }
 
-        public bool ValidateCoordinates(out string errorMessage)
-        {
-            errorMessage = string.Empty;
+//            return true;
+//        }
 
-            if (_table.X < 0 || _table.Y < 0)
-            {
-                errorMessage = "Coordinates must be positive values!";
-                return false;
-            }
-            if (_table.X == _table.Y)
-            {
-                errorMessage = "Coordinates cannot be the same value!";
-                return false;
-            }
+//        public bool ValidateCoordinates(out string errorMessage)
+//        {
+//            errorMessage = string.Empty;
+
+//            if (_table.X < 0 || _table.Y < 0)
+//            {
+//                errorMessage = "Coordinates must be positive values!";
+//                return false;
+//            }
+
+//            if (_table.X == _table.Y)
+//            {
+//                errorMessage = "Coordinates cannot be the same value!";
+//                return false;
+//            }
 
 
-            if (_table.X > 1000 || _table.Y > 1000)
-            {
-                errorMessage = "Coordinates exceed layout boundaries!";
-                return false;
-            }
+//            if (_table.X > 1000 || _table.Y > 1000)
+//            {
+//                errorMessage = "Coordinates exceed layout boundaries!";
+//                return false;
+//            }
 
-            return true;
-        }
+//            return true;
+//        }
 
-   
-=======
-    public static class TableValidator
-    {
-        // Method to validate the table fields
-        public static Dictionary<string, List<string>> ValidateFields(Table table, List<Table> existingTables)
-        {
-            var errors = new Dictionary<string, List<string>>();
 
-            // Validate Table Number
-            if (table.Table_Number <= 0)
-            {
-                AddError(errors, nameof(table.Table_Number), "Table number must be greater than zero.");
-            }
-            else if (existingTables.Any(t => t.Table_Number == table.Table_Number))
-            {
-                AddError(errors, nameof(table.Table_Number), "Table number already exists!");
-            }
 
-            // Validate Coordinates (X and Y)
-            if (table.X == 0 && table.Y == 0)
-            {
-                AddError(errors, nameof(table.X), "Table coordinates cannot be (0, 0).");
-            }
+//        public static class TableValidator
+//        {
+//            // Method to validate the table fields
+//            public static Dictionary<string, List<string>> ValidateFields(Table table, List<Table> existingTables)
+//            {
+//                var errors = new Dictionary<string, List<string>>();
 
-            // Table Status Validation (optional if you want to validate it further)
-            if (table.Table_Status != true && table.Table_Status != false)
-            {
-                AddError(errors, nameof(table.Table_Status), "Table status is invalid.");
-            }
+//                // Validate Table Number
+//                if (table.Table_Number <= 0)
+//                {
+//                    AddError(errors, nameof(table.Table_Number), "Table number must be greater than zero.");
+//                }
+//                else if (existingTables.Any(t => t.Table_Number == table.Table_Number))
+//                {
+//                    AddError(errors, nameof(table.Table_Number), "Table number already exists!");
+//                }
 
-            return errors;
-        }
+//                // Validate Coordinates (X and Y)
+//                if (table.X == 0 && table.Y == 0)
+//                {
+//                    AddError(errors, nameof(table.X), "Table coordinates cannot be (0, 0).");
+//                }
 
-        // Helper method to add errors to the dictionary
-        private static void AddError(Dictionary<string, List<string>> dict, string key, string message)
-        {
-            if (!dict.ContainsKey(key))
-                dict[key] = new List<string>();
-            dict[key].Add(message);
-        }
->>>>>>> dab1703bf08ed3a06ba1f01870916b4e78507fdc
-    }
-}
+//                // Table Status Validation (optional if you want to validate it further)
+//                if (table.Table_Status != true && table.Table_Status != false)
+//                {
+//                    AddError(errors, nameof(table.Table_Status), "Table status is invalid.");
+//                }
+
+//                return errors;
+//            }
+
+//            // Helper method to add errors to the dictionary
+//            private static void AddError(Dictionary<string, List<string>> dict, string key, string message)
+//            {
+//                if (!dict.ContainsKey(key))
+//                    dict[key] = new List<string>();
+//                dict[key].Add(message);
+//            }
+
+//        }
+//    }
+//}
