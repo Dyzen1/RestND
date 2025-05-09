519a1b63a3ad9d03e73d5cec665081fb1c0af567
Dyzen1 <117169978+Dyzen1@users.noreply.github.com>
2025-05-09 20:57:56 +0300

    Need to commit before pull

--- RestND/Validations/ProductValidator.cs
+++ RestND/Validations/ProductValidator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RestND.Data;
+using RestND.MVVM.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,50 @@ using System.Threading.Tasks;
 
 namespace RestND.Validations
 {
-    internal class ProductValidation
+    internal class ProductValidator
     {
+        private readonly Product _product;
+        private readonly ProductService _productService;
+
+        public ProductValidator(Product product)
+        {
+            _product = product;
+            _productService = new ProductService();
+        }
+
+        public bool ValidateProductName(out string errorMessage)
+        {
+            errorMessage = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(_product.Product_Name))
+            {
+                errorMessage = "Product name cannot be empty!";
+                return false;
+            }
+
+            List<Product> products = _productService.GetAll();
+
+            if (products.Any(p => p.Product_Name == _product.Product_Name))
+            {
+                errorMessage = "Product with this name already exists!";
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidateQuantityAvailable(out string errorMessage)
+        {
+            errorMessage = string.Empty;
+
+            if (_product.Quantity_Available < 0)
+            {
+                errorMessage = "Quantity must be 0 or greater!";
+                return false;
+            }
+
+            return true;
+        }
+
     }
 }
--- RestND/Validations/TableValidator.cs
+++ RestND/Validations/TableValidator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RestND.Data;
+using RestND.MVVM.Model.Tables;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,64 @@ using System.Threading.Tasks;
 
 namespace RestND.Validations
 {
-    internal class TableValidator
+    public class TableValidator
     {
+
+        private readonly Table _table;
+        private readonly TableServices _tableService;
+
+        public TableValidator(Table table)
+        {
+            _table = table;
+            _tableService = new TableServices();
+        }
+
+        public bool ValidateTableNumber(out string errorMessage)
+        {
+            errorMessage = string.Empty;
+
+            if (_table.Table_Number <= 0)
+            {
+                errorMessage = "Table number must be greater than 0!";
+                return false;
+            }
+
+            List<Table> existingTables = _tableService.GetAll();
+
+            if (existingTables.Any(t => t.Table_Number == _table.Table_Number && t.Table_ID != _table.Table_ID))
+            {
+                errorMessage = "Table number already exists!";
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidateCoordinates(out string errorMessage)
+        {
+            errorMessage = string.Empty;
+
+            if (_table.X < 0 || _table.Y < 0)
+            {
+                errorMessage = "Coordinates must be positive values!";
+                return false;
+            }
+            if (_table.X == _table.Y)
+            {
+                errorMessage = "Coordinates cannot be the same value!";
+                return false;
+            }
+
+
+            if (_table.X > 1000 || _table.Y > 1000)
+            {
+                errorMessage = "Coordinates exceed layout boundaries!";
+                return false;
+            }
+
+            return true;
+        }
+
+   
     }
 }
