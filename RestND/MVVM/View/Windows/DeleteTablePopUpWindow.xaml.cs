7c37c189342dd5d70abf0e5525bb10771ebf1756
Dyzen1 <117169978+Dyzen1@users.noreply.github.com>
2025-06-15 22:55:30 +0300

    added delete edit for table also i added combo box styling

---
+++ RestND/MVVM/View/Windows/DeleteTablePopUpWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using RestND.MVVM.ViewModel.Main;
+
+namespace RestND.MVVM.View.Windows
+{
+    public partial class DeleteTablePopUpWindow : Window
+    {
+        private readonly MainWindowViewModel _mainVM;
+
+        public DeleteTablePopUpWindow()
+        {
+            InitializeComponent();
+            _mainVM = App.SharedMainVM;
+            this.DataContext = _mainVM;
+        }
+
+        private async void DeleteTableBtn_Click(object sender, RoutedEventArgs e)
+        {
+            await App.SharedMainVM.DeleteTableAsync();
+            this.Close();
+
+        }
+    }
+}
