ce5390ccd7295bb88a262d10a24092903432a399
Dyzen1 <117169978+Dyzen1@users.noreply.github.com>
2025-08-23 10:28:07 +0300

    employee

--- RestND/Validations/EmployeeValidator.cs
+++ RestND/Validations/EmployeeValidator.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 
 namespace RestND.Validations
 {
-    public class EmployeeValidator
+    public class EmployeeValidator : GeneralValidations
     {
         private readonly EmployeeServices _employeeService = new();
 
